477dd99514e80983a5de8783deaa3bb1c7d5ca57
JulianGrafen <juliangraefen@gmail.com>
2022-12-16 14:51:56 +0100

    Added styling to table

--- src/pages/EditCustomer.tsx
+++ src/pages/EditCustomer.tsx
@@ -1,8 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 export function EditCustomer() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3002/createCustomer")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  }, []);
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <table className="table-auto w-full">
+      <thead>
+        <tr className="bg-gray-200 text-xs font-semibold text-gray-700 uppercase tracking-wide">
+          <th className="px-4 py-2">Name</th>
+          <th className="px-4 py-2">Kennzeichen</th>
+          <th className="px-4 py-2">Status</th>
+          <th className="px-4 py-2">ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item: any) => (
+          <tr key={item.id} className="text-xs text-gray-700">
+            <td className="border px-4 py-2">{item.name}</td>
+            <td className="border px-4 py-2">{item.kennzeichen}</td>
+            <td className="border px-4 py-2">{item.status}</td>
+            <td className="border px-4 py-2">{item.id}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
