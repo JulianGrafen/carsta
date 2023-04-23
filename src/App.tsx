8fdbc8c0651abca70a4b9f5497629f32e66059d4
JulianGrafen <juliangraefen@gmail.com>
2023-04-23 13:16:53 +0200

    Changed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import { EditCustomer } from "./pages/EditCustomer";
 import { Container } from "react-bootstrap";
 import { CreateCustomer } from "./pages/CreateCustomer";
 import { Navbar } from "./components/navbar";
 import { Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
+  const basename = "/carsta";
   return (
     <>
       <Container className="mb-10">
-        <Routes>
+        <Router basename={basename}>
           <Route path="/createcustomer" element={<CreateCustomer />} />
           <Route path="/EditCustomer" element={<EditCustomer />} />
-        </Routes>
+        </Router>
       </Container>
     </>
   );
